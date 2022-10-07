c178fc6f6a1cebb922f3c72919eac8bb75fc80a4
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-10-07 15:38:44 -0300

    update

--- src/main/groovy/FristScraping.groovy
+++ src/main/groovy/FristScraping.groovy
@@ -4,6 +4,31 @@ class FristScraping {
   
   static fristScraping() {
         //1 - URL do site a ser acessado
+        String url = "https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss/padrao-tiss-2013-setembro-2022"
+        //2 - Conectando e obtendo uma cópia do html inteiro da página
+        Document doc = Jsoup.connect(url).get()
+        //3 - Obtendo a tabela através de sua classe
+        Element table = doc.getElementsByClass("table table-bordered").first()
+        //4 - Obtendo o corpo da tabela através da tag
+        Element tbody = table.getElementsByTag("tbody").first()
+        //5 - Criando uma lista de todos os tr's do tbody obtido
+        List<Element> players = tbody.getElementsByTag("tr")
+        //6 - Criando uma lista vazia para Guardar os dados dos tr's convertidos
+        List<Passing> playersObjects = new ArrayList<>()
+        //7 - Iterando por cada tr e convertendo-o em um Passing. Em seguida inserindo-o na lista de Passings
+        for (Element player: players) {
+            //Listando todos os atributos do tr em questão
+            List<Element> attributes = player.getElementsByTag("td");
+            //criando o objeto e inserindo nele os atributos extraidos do td
+            Passing passing = new Passing();
+            passing.setName(attributes.get(0).text());
+            passing.setPassYds(attributes.get(1).text());
+            passing.setYdsAtt(attributes.get(2).text());
+            passing.setAtt(attributes.get(3).text());
+            //adicionando o objeto na lista de objetos Passing
+            playersObjects.add(passing);
+        
+        //1 - URL do site a ser acessado
         String url = "https://jovemnerd.com.br/";
         //2 - Conectando e obtendo uma cópia do html inteiro da página
         Document doc = Jsoup.connect(url).get();
