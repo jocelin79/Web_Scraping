2b63128bd950215d54800e70780a1c7265b57dbe
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-10-07 15:32:33 -0300

    update

--- src/main/groovy/FristScraping.groovy
+++ src/main/groovy/FristScraping.groovy
@@ -1,3 +1,30 @@
 class FristScraping {
+  
+  static class InvalidFileTypeException extends Exception {}
+  
+  static fristScraping() {
+        //1 - URL do site a ser acessado
+        String url = "https://jovemnerd.com.br/";
+        //2 - Conectando e obtendo uma cópia do html inteiro da página
+        Document doc = Jsoup.connect(url).get();
+        //3 - Obtendo os artigos por classe
+        List<Element> artigos = doc.getElementsByClass("info");
 
+        List<Element> as = new ArrayList<>();
+        List<String> hrefs = new ArrayList<>();
+
+        //4 - Obtendo as tags "a" dos artigos
+        artigos.forEach(element -> {
+            as.add(element.getElementsByTag("a").first());
+        });
+
+        //5 - Obtendo as urls das tags a
+        as.forEach(element -> {
+            hrefs.add(element.attr("href"));
+        });
+        //6 - Imprimindo os links
+       hrefs.forEach(s -> {
+           System.out.println("URL: "  + s);
+       });
+  }    
 }
